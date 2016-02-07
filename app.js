be4c52363f102ff64b26d91fc92357f580f3bbfb
Mike Ulvila <mikeulvila@me.com>
2016-02-07 11:35:22 -0600

    added about.js to start routing

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var nodeMailer = require('nodemailer');
+
+// routes
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var about = require('./routes/about');
 
 var app = express();
 
@@ -23,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/about', about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/about.js
+++ routes/about.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
 });
 
 module.exports = router;
